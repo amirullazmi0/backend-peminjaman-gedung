f3ab2c2100b4dba0952f700955b91bd64743a2f4
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-02 17:35:06 +0700

    feat: implement authentication controller with login, register, and refresh-token endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Request, Response } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService
+  ) { }
+  @Post('login')
+  async Login() {
+
+  }
+
+  @Post('register')
+  async Register() {
+
+  }
+
+  @Post('refresh-token')
+  async refreshToken(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+    return this.authService.refreshToken(req, res);
+  }
+}
