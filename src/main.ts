d5439bd97be3230f674674b7e8b00d7cd76773d3
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-02 17:35:01 +0700

    feat: add global validation pipe to enhance request validation

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.useStaticAssets(join(__dirname, '..', 'public'));
   const env = app.get(ConfigService);
