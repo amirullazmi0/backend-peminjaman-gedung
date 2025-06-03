a663e17865cdb761d441cee902215f937edd7785
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-03 16:23:03 +0700

    fix: refactor AuthModule to include missing providers and improve structure

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { SendEmailService } from 'src/send-email/send-email.service';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  providers: [AuthService, PrismaService, SendEmailService],
+  controllers: [AuthController],
+  exports: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
