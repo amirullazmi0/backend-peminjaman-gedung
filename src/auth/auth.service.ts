f2d1c7a6c117d5b95860725092cd3b0418c0e1e9
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-03 16:23:09 +0700

    feat: implement user registration with email activation and validation checks

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,21 @@
 import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { authForgetPasswordDto, authForgetPasswordResponseDto, authLoginRequestDto, authLoginResponse } from './AuthDTO';
+import { authForgetPasswordDto, authForgetPasswordResponseDto, authLoginRequestDto, authLoginResponse, authRegisterRequestDto, authRegisterResponse } from './AuthDTO';
 import { WebResponse } from 'src/DTO/globalsResponse';
-import { authLoginFailed, authLoginSuccess, emailPassworWrong, urlNewPasswordSuccess, userNotActive } from 'src/DTO/messages';
+import { authLoginFailed, authLoginSuccess, emailIsUnique, emailPassworWrong, phoneIsUnique, registerSuccess, urlNewPasswordSuccess, userNotActive } from 'src/DTO/messages';
 import { User } from 'generated/prisma';
 import * as bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
+import { SendEmailService } from 'src/send-email/send-email.service';
+import { formatIndonesianDate } from 'src/utils/formatDate';
 
 @Injectable()
 export class AuthService {
   constructor(
     private prismaService: PrismaService,
     private jwtService: JwtService,
+    private sendEmailService: SendEmailService
   ) { }
 
   async login(req: authLoginRequestDto, res: Response): Promise<WebResponse<authLoginResponse>> {
@@ -61,6 +64,75 @@ export class AuthService {
         accessToken: accessToken,
       }
     };
+  }
+
+  async register(body: authRegisterRequestDto): Promise<WebResponse<authRegisterResponse>> {
+    let user = await this.findUserByEmail(body.email)
+
+    if (user) {
+      throw new BadRequestException(emailIsUnique)
+    }
+
+    const userByPhone = await this.findUserByPhone(body.phone)
+
+    if (!userByPhone) {
+      throw new BadRequestException(phoneIsUnique)
+    }
+
+    const bcryptPassword = await bcrypt.hash(body.password, 10);
+
+    user = await this.prismaService.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        phone: body.phone,
+        role: body.role,
+        password: bcryptPassword
+      }
+    })
+
+    await this.prismaService.userAddress.create({
+      data: {
+        userId: user.id
+      }
+    })
+
+    const activationToken = await this.jwtService.sign(
+      {
+        email: user.email,
+        role: user.role,
+        type: 'activation'
+      },
+      { expiresIn: '1h' }
+    );
+
+    const ORIGIN_FE = process.env.ORIGIN_FE
+    const oneHourSoon = new Date(Date.now() + 60 * 60 * 1000);
+
+    await this.sendEmailService.sendEmail({
+      subject: 'Aktivasi Akun',
+      type: 'account-activation',
+      to: user.email,
+      context: {
+        name: user.name,
+        email: user.email,
+        url: `${ORIGIN_FE}/auth/${activationToken}/activation-user`,
+        date: formatIndonesianDate(oneHourSoon)
+      }
+    })
+
+    return {
+      success: true,
+      message: registerSuccess,
+      data: {
+        name: user.name,
+        email: user.email
+      }
+    }
+  }
+
+  async activatedUser() {
+
   }
 
   async refreshToken(req: Request, res: Response) {
@@ -133,6 +205,28 @@ export class AuthService {
         id: id
       }
     })
+
+    if (user) {
+      return {
+        user: user
+      }
+    } else {
+      return {
+        user: undefined
+      }
+    }
+  }
+
+  async findUserByPhone(phone: string): Promise<{ user: User }> {
+
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        phone: phone
+      }
+    })
+
+    console.log('user_________________________', user);
+
 
     if (user) {
       return {
