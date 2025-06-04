9adceef73013719f324ee264436d5cb96550633d
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-04 14:58:58 +0700

    refactor: streamline Auth decorator and improve error message consistency

--- src/common/auth.decorator.ts
+++ src/common/auth.decorator.ts
@@ -1,23 +1,18 @@
-import {
-  createParamDecorator,
-  ExecutionContext,
-  HttpException,
-  HttpStatus,
-  //   Next,
-} from '@nestjs/common';
-import { User } from 'generated/prisma';
+import { createParamDecorator, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
+import { Role, User } from 'generated/prisma';
 
-
-export const Auth = createParamDecorator(
+const AuthRole = (roles: Role[]) => createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-
     const user: User = request.user;
-
     if (user) {
+      if (!roles.includes(user.role)) {
+        throw new HttpException('Access Denied: Insufficient Role', HttpStatus.FORBIDDEN);
+      }
       return user;
     } else {
-      throw new HttpException('unAuthorized', HttpStatus.UNAUTHORIZED);
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
     }
   },
 );
+export const Auth = AuthRole(['ADMIN', 'SUPERADMIN', 'USER']);
