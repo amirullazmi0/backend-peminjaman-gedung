e5a6f8e6e72d0d5d726171d1ccdf3749142d1829
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-04 14:59:07 +0700

    refactor: improve AuthMiddleware by replacing token handling with JWT verification and enhancing user retrieval logic

--- src/common/auth.middleware.ts
+++ src/common/auth.middleware.ts
@@ -1,37 +1,37 @@
-import {
-  Injectable,
-  NestMiddleware,
-  //   UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, NestMiddleware, HttpException, HttpStatus } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { tokenVerify } from 'src/auth/AuthDTO';
 
 @Injectable()
-export class AuthUserMidlleware implements NestMiddleware {
-  constructor(private prismaService: PrismaService) { }
+export class AuthMiddleware implements NestMiddleware {
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) { }
 
   async use(req: any, res: any, next: (error?: any) => void) {
     const [type, token] = req.headers.authorization?.split(' ') ?? [];
-
     if (type === 'Bearer' && token) {
-      let user = await this.prismaService.user.findFirst({
-        where: { accessToken: token },
-      });
-
-      if (!user) {
-        user = await this.prismaService.user.findFirst({
-          where: { refreshToken: token },
-        });
-      }
-
-      if (user) {
-        user = await this.prismaService.user.update({
-          where: { id: user.id },
-          data: { lastActive: new Date() },
-        });
-        req.user = user;
+      try {
+        const decoded: tokenVerify = this.jwtService.verify(token);
+        if (decoded && decoded.email) {
+          let user = await this.prismaService.user.findUnique({
+            where: { email: decoded.email },
+          });
+          if (user) {
+            user = await this.prismaService.user.update({
+              where: { id: user.id },
+              data: { lastActive: new Date() },
+            });
+            req.user = user;
+          }
+        }
+      } catch (error) {
+        console.error('Token verification failed', error);
+        throw new HttpException('Invalid token or expired', HttpStatus.UNAUTHORIZED);
       }
     }
-
     next();
   }
 }
