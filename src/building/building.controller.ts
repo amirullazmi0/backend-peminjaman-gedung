4d2e54a51ab46f7a3d19d0a590d579e4496b2cf6
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-05 16:51:53 +0700

    feat: implement BuildingController with CRUD operations and authentication guards

--- src/building/building.controller.ts
+++ src/building/building.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Auth, AuthUser } from 'src/common/auth.decorator';
+import { RolesGuard } from 'src/common/roles.guard';
+import { BuildingService } from './building.service';
+import { User } from 'generated/prisma';
+import { AddItemBuildingRequestDto } from './buildingDto';
 
-@Controller('building')
-export class BuildingController {}
+@Controller('api/building')
+export class BuildingController {
+  constructor(
+    private buildingService: BuildingService
+  ) { }
+  @Get(':id?')
+  async getAll(@Param('id') id?: string) {
+    return this.buildingService.getAll(id);
+  }
+
+  @Post('/add')
+  @Auth(['ADMIN', 'SUPERADMIN'])
+  @UseGuards(RolesGuard)
+  async create(
+    @AuthUser() user: User,
+    @Body() body?: AddItemBuildingRequestDto
+  ) {
+    return this.buildingService.create(user, body);
+  }
+} 
