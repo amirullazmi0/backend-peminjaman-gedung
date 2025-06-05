75994a4aee3484c1da85bc5a6e1b093286d44be4
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-05 16:52:26 +0700

    refactor: simplify Auth decorator by removing role checks and unnecessary imports

--- src/common/auth.decorator.ts
+++ src/common/auth.decorator.ts
@@ -1,18 +1,11 @@
-import { createParamDecorator, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
-import { Role, User } from 'generated/prisma';
+import { createParamDecorator, ExecutionContext, SetMetadata } from '@nestjs/common';
+import { Role } from 'generated/prisma';
 
-const AuthRole = (roles: Role[]) => createParamDecorator(
+export const Auth = (roles: Role[]) => SetMetadata('roles', roles);
+
+export const AuthUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    const user: User = request.user;
-    if (user) {
-      if (!roles.includes(user.role)) {
-        throw new HttpException('Access Denied: Insufficient Role', HttpStatus.FORBIDDEN);
-      }
-      return user;
-    } else {
-      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
-    }
-  },
+    return request.user;
+  }
 );
-export const Auth = AuthRole(['ADMIN', 'SUPERADMIN', 'USER']);
