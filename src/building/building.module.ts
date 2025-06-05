d2250c0d73e5b796d12ac7c130af0e71181874e8
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-05 16:51:58 +0700

    feat: add PrismaService to BuildingModule providers

--- src/building/building.module.ts
+++ src/building/building.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BuildingController } from './building.controller';
 import { BuildingService } from './building.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [BuildingController],
-  providers: [BuildingService]
+  providers: [BuildingService, PrismaService]
 })
-export class BuildingModule {}
+export class BuildingModule { }
