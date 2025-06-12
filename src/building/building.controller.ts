8d87c3e4734d46846a33c378a050a5b63eeecbf1
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-12 16:46:34 +0700

    feat: add image and document upload functionality to BuildingController

--- src/building/building.controller.ts
+++ src/building/building.controller.ts
@@ -1,16 +1,17 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, ParseFilePipeBuilder, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { Auth, AuthUser } from 'src/common/auth.decorator';
 import { RolesGuard } from 'src/common/roles.guard';
 import { BuildingService } from './building.service';
 import { User } from 'generated/prisma';
 import { AddItemBuildingRequestDto } from './buildingDto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('api/building')
 export class BuildingController {
   constructor(
     private buildingService: BuildingService
   ) { }
-  @Get(':id?')
+  @Get(':id')
   async getAll(@Param('id') id?: string) {
     return this.buildingService.getAll(id);
   }
@@ -23,5 +24,45 @@ export class BuildingController {
     @Body() body?: AddItemBuildingRequestDto
   ) {
     return this.buildingService.create(user, body);
+  }
+
+  @Post('image')
+  @UseInterceptors(FileInterceptor('file'))
+  @Auth(['ADMIN', 'SUPERADMIN', 'USER'])
+  @UseGuards(RolesGuard)
+  async saveImage(
+    @AuthUser() user: User,
+    @UploadedFile(
+      new ParseFilePipeBuilder().build({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        fileIsRequired: false,
+      }),
+    )
+    file?: Express.Multer.File,
+  ) {
+    return this.buildingService.saveBuildingImage({
+      user: user,
+      file: file,
+    })
+  }
+
+  @Post('document')
+  @UseInterceptors(FileInterceptor('file'))
+  @Auth(['ADMIN', 'SUPERADMIN', 'USER'])
+  @UseGuards(RolesGuard)
+  async saveDocument(
+    @AuthUser() user: User,
+    @UploadedFile(
+      new ParseFilePipeBuilder().build({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        fileIsRequired: false,
+      }),
+    )
+    file?: Express.Multer.File,
+  ) {
+    return this.buildingService.saveBuildingDocument({
+      user: user,
+      file: file,
+    })
   }
 } 
