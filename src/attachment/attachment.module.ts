3903674a0b1b372efc9f6b911914d62e04419918
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-12 16:46:17 +0700

    fix: add AttachmentController to AttachmentModule

--- src/attachment/attachment.module.ts
+++ src/attachment/attachment.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AttachmentService } from './attachment.service';
+import { AttachmentController } from './attachment.controller';
 
 @Module({
-  providers: [AttachmentService]
+  providers: [AttachmentService],
+  controllers: [AttachmentController],
 })
-export class AttachmentModule {}
+export class AttachmentModule { }
