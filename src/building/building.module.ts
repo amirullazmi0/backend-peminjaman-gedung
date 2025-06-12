ef68ef0cd991da58e40453d45ff377b30c02b7fb
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-12 16:46:38 +0700

    feat: add AttachmentService to BuildingModule providers

--- src/building/building.module.ts
+++ src/building/building.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BuildingController } from './building.controller';
 import { BuildingService } from './building.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AttachmentService } from 'src/attachment/attachment.service';
 
 @Module({
   controllers: [BuildingController],
-  providers: [BuildingService, PrismaService]
+  providers: [BuildingService, PrismaService, AttachmentService]
 })
 export class BuildingModule { }
