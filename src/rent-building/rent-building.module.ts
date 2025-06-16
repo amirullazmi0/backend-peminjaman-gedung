0bc24beafc4844e609d6b8c600863d3c63d588f6
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-17 03:01:39 +0700

    feat: add PrismaService and InvoiceService to RentBuildingModule providers

--- src/rent-building/rent-building.module.ts
+++ src/rent-building/rent-building.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RentBuildingController } from './rent-building.controller';
 import { RentBuildingService } from './rent-building.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { InvoiceService } from 'src/invoice/invoice.service';
 
 @Module({
   controllers: [RentBuildingController],
-  providers: [RentBuildingService]
+  providers: [RentBuildingService, PrismaService, InvoiceService]
 })
-export class RentBuildingModule {}
+export class RentBuildingModule { }
