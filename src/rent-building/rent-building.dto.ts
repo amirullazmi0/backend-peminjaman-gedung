7e3e2a98cace3ef3b3c4146e1f4477f8ea0298d2
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-17 15:05:08 +0700

    feat: add DTOs for rent building operations including create, update, and delete requests

---
+++ src/rent-building/rent-building.dto.ts
@@ -0,0 +1,51 @@
+import { Type } from "class-transformer";
+import { IsArray, IsDateString, IsNotEmpty, IsString, ValidateNested } from "class-validator";
+import { RentStatus } from "generated/prisma";
+
+export class DeleteRentBuildingRequestDto {
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+}
+
+export class CreateRentBuildingRequestDto {
+  @IsNotEmpty()
+  @IsString()
+  buildingId: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  startDate: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  endDate: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => SupportDocumentRequirementDto) // ✅ benar
+  supportDocumentRequirements: SupportDocumentRequirementDto[];
+}
+
+
+export class SupportDocumentRequirementDto {
+  @IsNotEmpty()
+  @IsString()
+  supportDocumentId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  documentUrl: string;
+}
+export class UpdateRentBuildingRequestDto {
+  @IsNotEmpty()
+  @IsString()
+  id: string
+
+  @IsNotEmpty()
+  @IsString()
+  status: RentStatus
+}
+
+
+
