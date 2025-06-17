e5e738bcb206dfe444947ae68af4f92d71047abc
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-17 15:05:17 +0700

    feat: enhance RentBuildingService with user-specific retrieval and document handling

--- src/rent-building/rent-building.service.ts
+++ src/rent-building/rent-building.service.ts
@@ -3,14 +3,16 @@ import { Building, RentBuilding, User } from 'generated/prisma';
 import { WebResponse } from 'src/DTO/globalsResponse';
 import { dataNotFound, deleteDataSuccess, getDataSuccess, postDataSuccess, updateDataSuccess } from 'src/DTO/messages';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateRentBuildingRequestDto, DeleteRentBuildingRequestDto, UpdateRentBuildingRequestDto } from './ren-building.dto';
+import { CreateRentBuildingRequestDto, DeleteRentBuildingRequestDto, UpdateRentBuildingRequestDto } from './rent-building.dto';
 import { InvoiceService } from 'src/invoice/invoice.service';
+import { AttachmentService } from 'src/attachment/attachment.service';
 
 @Injectable()
 export class RentBuildingService {
   constructor(
     private prismaService: PrismaService,
-    private invoiceService: InvoiceService
+    private invoiceService: InvoiceService,
+    private attachmentService: AttachmentService
   ) { }
 
   async getAll(id?: string): Promise<WebResponse<RentBuilding | RentBuilding[]>> {
@@ -47,6 +49,63 @@ export class RentBuildingService {
     }
   }
 
+  async getAllByUser(user: User): Promise<WebResponse<RentBuilding | RentBuilding[]>> {
+    let rentBuilding: RentBuilding | RentBuilding[] = await this.prismaService.rentBuilding.findMany({
+      where: {
+        userId: user.id,
+        deletedAt: null
+      },
+      include: {
+        _count: true,
+        building: {
+          select: {
+            buildingPhoto: true,
+            name: true
+          },
+        },
+        invoice: true
+      },
+      orderBy: {
+        createdAt: 'desc'
+      }
+    })
+
+    return {
+      success: true,
+      message: getDataSuccess,
+      data: rentBuilding
+    }
+  }
+
+  async getAllByAdmin(user: User): Promise<WebResponse<RentBuilding | RentBuilding[]>> {
+    let rentBuilding: RentBuilding | RentBuilding[] = await this.prismaService.rentBuilding.findMany({
+      include: {
+        _count: true,
+        building: {
+          select: {
+            buildingPhoto: true,
+            name: true
+          },
+        },
+        invoice: true
+      },
+      where: {
+        building: {
+          userId: user.id
+        }
+      },
+      orderBy: {
+        createdAt: 'desc'
+      }
+    })
+
+    return {
+      success: true,
+      message: getDataSuccess,
+      data: rentBuilding
+    }
+  }
+
   async create(body: CreateRentBuildingRequestDto, user: User): Promise<WebResponse<RentBuilding>> {
     const building = await this.getBuildingById(body.buildingId)
 
@@ -64,6 +123,20 @@ export class RentBuildingService {
         createdBy: user.id
       }
     })
+
+    if (body.supportDocumentRequirements.length > 0) {
+      body.supportDocumentRequirements.map(async (ss) => {
+        await this.prismaService.supportDocumentRentBuilding.create({
+          data: {
+            supportDocumentRequirementId: ss.supportDocumentId,
+            documentUrl: ss.documentUrl,
+            rentBuildingId: rent.id,
+            createdBy: user.id
+          }
+        })
+      })
+
+    }
 
     return {
       success: true,
@@ -104,32 +177,32 @@ export class RentBuildingService {
       }
     })
 
-    if (body.status === 'SUCCESS') {
-      const address = `${rent.building.buildingAddress[0].jalan}, RT ${rent.building.buildingAddress[0].rt}, RW ${rent.building.buildingAddress[0].rw},${rent.building.buildingAddress[0].kelurahan}, ${rent.building.buildingAddress[0].kecamatan}, ${rent.building.buildingAddress[0].kota}, ${rent.building.buildingAddress[0].provinsi} ${rent.building.buildingAddress[0].kodepos}`
+    // if (body.status === 'SUCCESS') {
+    //   const address = `${rent.building.buildingAddress[0].jalan}, RT ${rent.building.buildingAddress[0].rt}, RW ${rent.building.buildingAddress[0].rw},${rent.building.buildingAddress[0].kelurahan}, ${rent.building.buildingAddress[0].kecamatan}, ${rent.building.buildingAddress[0].kota}, ${rent.building.buildingAddress[0].provinsi} ${rent.building.buildingAddress[0].kodepos}`
 
-      const generateInvoice = await this.invoiceService.generateInvoice({
-        buidingName: rent.building.name,
-        buildingAddress: address,
-        buildingUserEmail: rent.building.user.email,
-        buildingUserName: rent.building.user.email,
-        buildingUserPhone: rent.building.user.phone,
-        userName: rent.user.name,
-        userEmail: rent.user.email,
-        userPhone: rent.user.phone,
-        price: rent.building.price,
-        startDate: rent.startDate,
-        endDate: rent.endDate
-      })
+    //   const generateInvoice = await this.invoiceService.generateInvoice({
+    //     buidingName: rent.building.name,
+    //     buildingAddress: address,
+    //     buildingUserEmail: rent.building.user.email,
+    //     buildingUserName: rent.building.user.email,
+    //     buildingUserPhone: rent.building.user.phone,
+    //     userName: rent.user.name,
+    //     userEmail: rent.user.email,
+    //     userPhone: rent.user.phone,
+    //     price: rent.building.price,
+    //     startDate: rent.startDate,
+    //     endDate: rent.endDate
+    //   })
 
-      await this.prismaService.invoice.create({
-        data: {
-          rentId: rent.id,
-          customId: generateInvoice.data.customId,
-          url: generateInvoice.data.invoiceUrl,
-          createdBy: user.id
-        }
-      })
-    }
+    //   await this.prismaService.invoice.create({
+    //     data: {
+    //       rentId: rent.id,
+    //       customId: generateInvoice.data.customId,
+    //       url: generateInvoice.data.invoiceUrl,
+    //       createdBy: user.id
+    //     }
+    //   })
+    // }
 
     return {
       success: true,
@@ -179,6 +252,31 @@ export class RentBuildingService {
     return {
       message: deleteDataSuccess,
       success: true
+    }
+  }
+
+  async saveRentDocument({
+    user,
+    file,
+  }: {
+    user: User
+    file: Express.Multer.File;
+  }): Promise<WebResponse<{
+    url: string
+  }>> {
+    const imageSave = await this.attachmentService.saveDocumentImageKit({
+      file: file,
+      folder: `/${user.id}`
+    })
+
+    const url = imageSave.path;
+
+    return {
+      success: true,
+      message: 'File uploaded successfully',
+      data: {
+        url: url
+      }
     }
   }
 }
