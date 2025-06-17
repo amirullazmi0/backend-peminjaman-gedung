fd5e75bc3801b3daa5d061a4bc9e34ced73a37f1
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-17 23:53:50 +0700

    feat: add update and delete operations to BuildingController with appropriate DTOs

--- src/building/building.controller.ts
+++ src/building/building.controller.ts
@@ -1,9 +1,9 @@
-import { Body, Controller, Get, HttpStatus, Param, ParseFilePipeBuilder, Post, Query, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, ParseFilePipeBuilder, Patch, Post, Query, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { Auth, AuthUser } from 'src/common/auth.decorator';
 import { RolesGuard } from 'src/common/roles.guard';
 import { BuildingService } from './building.service';
 import { User } from 'generated/prisma';
-import { AddItemBuildingRequestDto } from './buildingDto';
+import { AddItemBuildingRequestDto, deleteBuildingRequestDto, updateBuildingRequestDto } from './buildingDto';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('api/building')
@@ -29,14 +29,33 @@ export class BuildingController {
   }
 
   @Post()
-  @Auth(['ADMIN', 'SUPERADMIN'])
+  @Auth(['ADMIN'])
   @UseGuards(RolesGuard)
   async create(
     @AuthUser() user: User,
     @Body() body?: AddItemBuildingRequestDto
   ) {
-    console.log('body cpost', body);
     return this.buildingService.create(user, body);
+  }
+
+  @Patch()
+  @Auth(['ADMIN', 'SUPERADMIN'])
+  @UseGuards(RolesGuard)
+  async update(
+    @AuthUser() user: User,
+    @Body() body?: updateBuildingRequestDto
+  ) {
+    return this.buildingService.update(user, body);
+  }
+
+  @Delete()
+  @Auth(['ADMIN', 'SUPERADMIN'])
+  @UseGuards(RolesGuard)
+  async delete(
+    @AuthUser() user: User,
+    @Body() body?: deleteBuildingRequestDto
+  ) {
+    return this.buildingService.delete(user, body);
   }
 
   @Post('image')
