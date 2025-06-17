5a70d472fe5affc29770c295db33069820e77aac
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-17 15:05:03 +0700

    feat: implement RentBuildingController with CRUD operations and document handling

--- src/rent-building/rent-building.controller.ts
+++ src/rent-building/rent-building.controller.ts
@@ -1,4 +1,76 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, ParseFilePipeBuilder, Patch, Post, Query, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { RentBuildingService } from './rent-building.service';
+import { Auth, AuthUser } from 'src/common/auth.decorator';
+import { CreateRentBuildingRequestDto, UpdateRentBuildingRequestDto } from './rent-building.dto';
+import { User } from 'generated/prisma';
+import { RolesGuard } from 'src/common/roles.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
-@Controller('rent-building')
-export class RentBuildingController {}
+@Controller('api/rent-building')
+export class RentBuildingController {
+
+  constructor(
+    private rentBuildingService: RentBuildingService
+  ) { }
+
+  @Get()
+  async getAll(
+    @Query('id') id?: string
+  ) {
+    return this.rentBuildingService.getAll(id);
+  }
+
+  @Post()
+  @Auth(['USER'])
+  @UseGuards(RolesGuard)
+  async create(
+    @AuthUser() user: User,
+    @Body() body?: CreateRentBuildingRequestDto
+  ) {
+    return this.rentBuildingService.create(body, user);
+  }
+
+  @Patch()
+  @Auth(['ADMIN'])
+  @UseGuards(RolesGuard)
+  async update(
+    @AuthUser() user: User,
+    @Body() body?: UpdateRentBuildingRequestDto
+  ) {
+    return this.rentBuildingService.update(body, user);
+  }
+
+  @Get('/user')
+  @Auth(['USER'])
+  @UseGuards(RolesGuard)
+  async getAllByUser(@AuthUser() user: User) {
+    return this.rentBuildingService.getAllByUser(user);
+  }
+
+  @Get('/admin')
+  @Auth(['ADMIN'])
+  @UseGuards(RolesGuard)
+  async getAllByAdmin(@AuthUser() user: User) {
+    return this.rentBuildingService.getAllByAdmin(user);
+  }
+
+  @Post('document')
+  @UseInterceptors(FileInterceptor('file'))
+  @Auth(['ADMIN', 'SUPERADMIN', 'USER'])
+  @UseGuards(RolesGuard)
+  async saveDocument(
+    @AuthUser() user: User,
+    @UploadedFile(
+      new ParseFilePipeBuilder().build({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        fileIsRequired: false,
+      }),
+    )
+    file?: Express.Multer.File,
+  ) {
+    return this.rentBuildingService.saveRentDocument({
+      user: user,
+      file: file,
+    })
+  }
+}
