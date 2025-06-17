5291556ce30f51b4feb56e21255fe6f4ac0c2f6a
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-17 15:05:12 +0700

    feat: add AttachmentService to RentBuildingModule providers

--- src/rent-building/rent-building.module.ts
+++ src/rent-building/rent-building.module.ts
@@ -3,9 +3,10 @@ import { RentBuildingController } from './rent-building.controller';
 import { RentBuildingService } from './rent-building.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { InvoiceService } from 'src/invoice/invoice.service';
+import { AttachmentService } from 'src/attachment/attachment.service';
 
 @Module({
   controllers: [RentBuildingController],
-  providers: [RentBuildingService, PrismaService, InvoiceService]
+  providers: [RentBuildingService, PrismaService, InvoiceService, AttachmentService]
 })
 export class RentBuildingModule { }
