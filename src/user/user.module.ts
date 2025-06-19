19105c85c52a90c232995cbb3a9717d37c46f47c
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-20 00:42:47 +0700

    feat: add PrismaService and AttachmentService to UserModule providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AttachmentService } from 'src/attachment/attachment.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, PrismaService, AttachmentService]
 })
 export class UserModule { }
